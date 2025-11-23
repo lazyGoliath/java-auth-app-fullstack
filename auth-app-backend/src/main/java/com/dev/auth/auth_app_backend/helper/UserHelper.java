ad51b096736a5257a60ead0864dc962385805448
Bhavesh Shastri <lazygolith@gmail.com>
2025-11-23 16:07:39 +0530

    Adding helper class to parse user id from String to UUID type

---
+++ auth-app-backend/src/main/java/com/dev/auth/auth_app_backend/helper/UserHelper.java
@@ -0,0 +1,10 @@
+package com.dev.auth.auth_app_backend.helper;
+
+import java.util.UUID;
+
+public class UserHelper {
+
+    public static UUID parseUUID(String uuid) {
+        return UUID.fromString(uuid);
+    }
+}
