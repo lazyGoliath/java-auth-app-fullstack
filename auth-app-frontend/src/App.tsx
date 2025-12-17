0ea8187d5131e3874e73db06293bc6a61bf06648
Bhavesh Shastri <lazygolith@gmail.com>
2025-12-17 20:15:30 +0530

    Add shadCN components and configure 'ract-router' library

--- auth-app-frontend/src/App.tsx
+++ auth-app-frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css'
+import { Button } from './components/ui/button'
+import { Calendar } from './components/ui/calendar'
 
 function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold">Hello, Auth App Frontend</h1>
+      <Button variant={'outline'}>Click Me</Button>
+      <Calendar />
     </div>
   )
 }
